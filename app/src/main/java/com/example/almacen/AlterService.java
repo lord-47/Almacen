40006f83af21c9dce03890b1bca49096cd073fb0
Hugo <hugo_cg6@outlook.com>
2022-12-04 12:17:25 +0100

    Nueva funcionalidad

---
+++ app/src/main/java/com/example/almacen/AlterService.java
@@ -0,0 +1,32 @@
+package com.example.almacen;
+
+import android.app.Service;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.IBinder;
+
+import androidx.annotation.Nullable;
+
+public class AlterService extends Service {
+    private MediaPlayer player;
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        player = MediaPlayer.create(this, R.raw.misicalofi);
+        player.setLooping(true);
+        player.start();
+        return START_STICKY;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        player.stop();
+    }
+}
--- app/src/main/java/com/example/almacen/menuSonido2.java
+++ app/src/main/java/com/example/almacen/menuSonido2.java
@@ -2,19 +2,31 @@ package com.example.almacen;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ActivityManager;
+import android.content.Context;
 import android.content.Intent;
 import android.media.AudioManager;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.SeekBar;
 
 public class menuSonido2 extends AppCompatActivity {
+    Button btnPlayChill;
+    Button btnPlayLofi;
     AudioManager gestorSonido;
+    Boolean isPlayingChill;
+    Boolean isPlayingLofi;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_sonido2);
         gestorSonido = (AudioManager) getSystemService(AUDIO_SERVICE);
+        btnPlayChill=findViewById(R.id.btnPlayChill);
+        btnPlayLofi=findViewById(R.id.btnPlayLofi);
+        isPlayingChill = isMyServiceRunning(MyService3.class);
+        isPlayingLofi = isMyServiceRunning(AlterService.class);
 
         int volumenMaximo = gestorSonido.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
         int volumenActual = gestorSonido.getStreamVolume(AudioManager.STREAM_MUSIC);
@@ -38,11 +50,52 @@ public class menuSonido2 extends AppCompatActivity {
 
             }
         });
+
+        btnPlayChill.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(isPlayingLofi){
+                    stopService(new Intent(menuSonido2.this, AlterService.class));
+                    isPlayingLofi =false;
+                    startService(new Intent(menuSonido2.this, MyService3.class));
+                    isPlayingChill=true;
+                }else {
+                    startService(new Intent(menuSonido2.this, MyService3.class));
+                    isPlayingChill = true;
+                }
+            }
+        });
+
+        btnPlayLofi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(isPlayingChill){
+                    stopService(new Intent(menuSonido2.this, MyService3.class));
+                    isPlayingChill =false;
+                    startService(new Intent(menuSonido2.this, AlterService.class));
+                    isPlayingLofi=true;
+                }else {
+                    startService(new Intent(menuSonido2.this, AlterService.class));
+                    isPlayingLofi = true;
+                }
+            }
+        });
+
     }
 
     public void menu(View view) {
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
+    }
+
+    private boolean isMyServiceRunning(Class<?> serviceClass) {
+        ActivityManager manager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
+        for (ActivityManager.RunningServiceInfo service : manager.getRunningServices(Integer.MAX_VALUE)) {
+            if (serviceClass.getName().equals(service.service.getClassName())) {
+                return true;
+            }
+        }
+        return false;
     }
 
 }
